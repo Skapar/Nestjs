32e1e68483eee00c45f8721455a70e69732ab461
Skapar <skaparadr@gmail.com>
2023-10-15 20:45:09 +0600

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { OrganizationModule } from './organization/organization.module';
 import { LessonModule } from './lesson/lesson.module';
@@ -11,7 +10,6 @@ import { SubscriptionTypeModule } from './subscription-type/subscription-type.mo
   imports: [
     AuthModule,
     UserModule,
-    BookmarkModule,
     PrismaModule,
     OrganizationModule,
     LessonModule,
