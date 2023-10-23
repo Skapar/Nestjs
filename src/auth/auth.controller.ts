0f2680b6493201c090b16b1e2c17c73295c8a7b7
Skapar <skaparadr@gmail.com>
2023-10-24 02:14:53 +0600

    Users_Finished

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,7 +6,7 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthDto } from './dto'
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
